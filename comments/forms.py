4598e4776cccc820266b337af85721e90d975cf3
igor.kulish <igor.kulish09@gmail.com>
2023-12-08 12:45:24 +0200

    last chenges

--- comments/forms.py
+++ comments/forms.py
@@ -1,10 +1,25 @@
 from django import forms
 from .models import Comment
 # from  captcha.forms import reCuatchaFields
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
 
 class CommentForm(forms.ModelForm):
     # captcha = ReCaptchaField()
 
     class Meta:
         model = Comment
-        fields = ['user_name', 'email', 'text']
+        fields = ['user_name', 'email', "captcha",'text']
+
+
+@deconstructible
+class FileSizeValidator:
+    def __init__(self, max_size):
+        self.max_size = max_size
+
+    def __call__(self, value):
+        if value.size > self.max_size * 1024:
+            raise ValidationError(f'Розмір файлу повинен бути не більше {self.max_size} кБ.')
+
+
+file = forms.FileField(validators=[FileSizeValidator(max_size=100)])
